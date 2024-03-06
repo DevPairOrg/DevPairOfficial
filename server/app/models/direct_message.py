5d4062cbb4a2137dd79524d8b277e65f16727f73
Minh Tran <minhttran93@gmail.com>
2024-03-05 22:45:36 -0800

    dm models with user info, create/update times, message log, convo id

--- server/app/models/direct_message.py
+++ server/app/models/direct_message.py
@@ -1,4 +1,5 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime, timezone
 
 from sqlalchemy.schema import ForeignKey
 
@@ -10,12 +11,19 @@ class DirectMessage(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     conversation_id = db.Column(db.Integer, ForeignKey(add_prefix_for_prod('direct_message_conversation_id')), nullable=False)
+    message = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'), nullable=False))
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    conversation = db.relationship('DirectMessageConversation', back_populates='direct_messages')
+    user = db.relationship('User', back_populates='direct_messages')
 
     def to_dict(self):
-        data = {
-            'id': self.id,
-            'message_text': self.message_text,
-            'created_at': self.created_at
+        return {
+            'conversationId': self.conversation_id,
+            'user': self.user.to_dict(),
+            'message': self.message,
+            'createdAt': self.created_at,
+            'updatedAt': self.updated_at
         }
-
-        return data
