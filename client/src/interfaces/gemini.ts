c680daf47f021afff7d18ab6fbff4318c8fa5349
Melinda Cortez <meli.matrix@outlook.com>
2024-04-15 16:33:44 -0700

    Update frontend to use the returned dictionary response

--- client/src/interfaces/gemini.ts
+++ client/src/interfaces/gemini.ts
@@ -1,9 +1,14 @@
 export interface parsedData {
     problemName: string | undefined;
     problemPrompt: string | undefined;
-    testCases: string | undefined;
+    testCases: TestCase[] | undefined;
     pythonUnitTest: string | undefined;
     jsUnitTest: string | undefined;
     defaultPythonFn: string | undefined;
     defaultJsFn: string | undefined;
+  }
+
+  interface TestCase {
+    INPUT: string;
+    OUTPUT: string;
   }
