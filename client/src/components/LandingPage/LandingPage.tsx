8b4f144672c2df2436da8e9b45010b6ea6b1759e
Minh Tran <minhttran93@gmail.com>
2024-04-22 11:55:40 -0700

    add python submission version -- figure out how to utilize proper indentation with json.stringify for python

--- client/src/components/LandingPage/LandingPage.tsx
+++ client/src/components/LandingPage/LandingPage.tsx
@@ -34,7 +34,27 @@ function LandingPage() {
         navigate('/code-collab');
     };
 
-    const createSubmissionOnLocal = async () => {
+    // ***** LANGUAGE CODES: 71 (python) 63 (javascript) 93(javascript but this one is unavailable on local) *****
+    // * Create a submission - use base64_encoded=false&wait=true - wait=true grabs the results and returns it right away instead returning a token for you to use to hit another route to retrieve the results
+    // TODO: Figure out how to utilize multiple input / outputs
+    // ? Must include Post Method, Auth Token/User/Content-Type
+    // ? Body must be JSON.stringify
+
+    // * (Start of important comments)
+    // !!! the function stdin is being parsed because although the function being passed in has parameters (a, b) it's actually function twoSum(input) where 'input' is a string intiially
+    // Therefore, we have to trim, split the input (stdin) into an array [a, b]
+    // Then parse them into integers and reassigning them into proper variables
+    // const a = parseInt(input[0]) and const b = parseInt(input[1])
+    // Finally we use those variables to match the parameters of function twoSum(a, b)
+    // !!! IMPORTANT !!! results will only return correctly if we add a console.log(twoSum(a,b))
+    // * (End of important comments)
+
+    // TODO: Figure out an efficient way to have gemini parse this for us intially for the test case, or for it to not have to trim/split/parse and how to do it for arrays
+    // TODO: Figure out how to capture console.logs - when console.log is added to the user function and is submitted, it returns in the key 'stdout' along with the return value
+    // Example: "stdout": "Test Console Log\n8"
+    // ? Maybe tell users to remove console.logs before finalizing their submission (create a 'run' button that is separate from 'submission')
+
+    const createJSSubmissionOnLocal = async () => {
         const url = 'http://146.190.61.177:2358/submissions/?base64_encoded=false&wait=true&fields=*';
         const options = {
             method: 'POST',
@@ -51,9 +71,52 @@ function LandingPage() {
                     console.log(twoSum(a, b));
 
                     function twoSum(a, b) {
+                        const sum = a + b
+                        console.log('Test Console Log', sum)
                         return a + b;
                     }`,
                 language_id: 63,
+                stdin: 'a=5 b=3',
+                expected_output: '8',
+            }),
+        };
+        try {
+            const response = await fetch(url, options as any);
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    // ! For Python: Must Include 'additional_files': sys
+    // ! This is so the 'import sys' in the source code actually works correctly
+    const createPySubmissionOnLocal = async () => {
+        const url = 'http://146.190.61.177:2358/submissions/?base64_encoded=false&wait=true&fields=*';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Auth-Token': 'minh',
+                'X-Auth-User': 'tran',
+                // 'X-Auth-Host': 'http://146.190.61.177:2358',
+            },
+            body: JSON.stringify({
+                additional_files: 'sys',
+                // !!! FOR PYTHON YOU HAVE TO USE THIS INDENTATION LMAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+                source_code: `
+import sys
+def two_sum(a, b):
+    sum_value = a + b
+    return sum_value
+
+input_data = sys.stdin.read().strip().split(' ')
+a = int(input_data[0].split('=')[1])
+b = int(input_data[1].split('=')[1])
+
+print(two_sum(a, b))
+`,
+                language_id: 71,
                 stdin: 'a=5 b=3',
                 expected_output: '8',
             }),
@@ -85,14 +148,18 @@ function LandingPage() {
             console.error(error);
         }
     };
+
     return (
         <>
             <main className="landing-page">
                 <button onClick={handleAbout} style={{ color: 'black' }}>
-                    INITIAL ABOUT TEST
+                    IS THE SERVER RUNNING?
                 </button>
-                <button onClick={createSubmissionOnLocal} style={{ color: 'black', backgroundColor: 'purple' }}>
-                    createSubmissionOnLocal
+                <button onClick={createJSSubmissionOnLocal} style={{ color: 'black', backgroundColor: 'blue' }}>
+                    Create JavaScript Submission
+                </button>
+                <button onClick={createPySubmissionOnLocal} style={{ color: 'black', backgroundColor: 'red' }}>
+                    Create Python Submission
                 </button>
                 <div className="landing-page-cool-image">
                     <div id="cat-one">
