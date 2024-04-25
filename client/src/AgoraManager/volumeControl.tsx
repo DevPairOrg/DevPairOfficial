acdb00dff0ee40efc928b6d71d54f53da4124167
Melinda Cortez <meli.matrix@outlook.com>
2024-04-25 08:21:13 -0700

    Remove unused import in volumeControl.tsx

--- client/src/AgoraManager/volumeControl.tsx
+++ client/src/AgoraManager/volumeControl.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useAgoraContext } from './agoraManager';
 import { useRemoteUsers } from 'agora-rtc-react';
 import './volumeControl.css';
