0f042f965ced8fb229ab8ee2e5e11b25d06b8027
Melinda Cortez <meli.matrix@outlook.com>
2024-04-27 23:49:22 -0700

    Refactor GeminiDSA component to remove unnecessary divs and Modal Provider

--- client/src/components/PairedRoom/Content/GeminiDSA.tsx
+++ client/src/components/PairedRoom/Content/GeminiDSA.tsx
@@ -1,33 +1,27 @@
-import { useAppSelector } from '../../../hooks';
-import { ModalProvider } from '../../../context/Modal/Modal';
-import IDE from '../../CodeMirror/CodeMirror';
+import { useAppSelector } from "../../../hooks";
+import IDE from "../../CodeMirror/CodeMirror";
 
 interface GeminiDSAProps {
-    channelName?: string
+  channelName?: string;
 }
 
+const GeminiDSA: React.FC<GeminiDSAProps> = ({ channelName }) => {
+  const geminiProblem = useAppSelector(
+    (state) => state.pairedContent.gemini.generatedProblem
+  );
 
-const GeminiDSA: React.FC<GeminiDSAProps> = ({channelName}) => {
-    const geminiProblem = useAppSelector((state) => state.pairedContent.gemini.generatedProblem);
-
-    return (
-        <>
-            <div id="ide-main-container">
-                <ModalProvider>
-                    <IDE
-                        problemName={geminiProblem?.problemName}
-                        problemPrompt={geminiProblem?.problemPrompt}
-                        testCases={geminiProblem?.testCases}
-                        pythonUnitTest={geminiProblem?.pythonUnitTest}
-                        jsUnitTest={geminiProblem?.jsUnitTest}
-                        defaultPythonFn={geminiProblem?.defaultPythonFn}
-                        defaultJsFn={geminiProblem?.defaultJsFn}
-                        channelName={channelName}
-                    />
-                </ModalProvider>
-            </div>
-        </>
-    );
+  return (
+      <IDE
+        problemName={geminiProblem?.problemName}
+        problemPrompt={geminiProblem?.problemPrompt}
+        testCases={geminiProblem?.testCases}
+        pythonUnitTest={geminiProblem?.pythonUnitTest}
+        jsUnitTest={geminiProblem?.jsUnitTest}
+        defaultPythonFn={geminiProblem?.defaultPythonFn}
+        defaultJsFn={geminiProblem?.defaultJsFn}
+        channelName={channelName}
+      />
+  );
 };
 
 export default GeminiDSA;
