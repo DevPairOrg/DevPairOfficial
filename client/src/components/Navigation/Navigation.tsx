3ec09a3da022522d56f1850cb1632832a3caffbd
Minh Tran <minhttran93@gmail.com>
2024-05-01 16:45:44 -0700

    update navbar to display according to home page or user profile, add contact us which opens user default emailing method with subject Contact Us!

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,9 +1,11 @@
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { RootState } from '../../store/index';
 import ProfileButton from './ProfileButton';
+import { useAppDispatch } from '../../hooks';
 import name from '../../assets/devpair-logos/svg/devpair-high-resolution-logo-transparent.svg';
 import './Navigation.css';
+import { logout } from '../../store/session';
+import { useAppSelector } from '../../hooks';
 
 interface NavigationProps {
     isLoaded: boolean;
@@ -11,7 +13,11 @@ interface NavigationProps {
 
 function Navigation({ isLoaded }: NavigationProps) {
     // Assuming `state.session.user` is of type User | null
-    const sessionUser = useSelector((state: RootState) => state.session.user);
+    const sessionUser = useAppSelector((state: RootState) => state.session.user);
+    const currentRoute = useAppSelector((state) => state.userPath.currentPath);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    console.log(currentRoute);
 
     //sessionUser is returning true even if there is no user logged in because it is returning the user object { errors: [] }
     //so we need to check if there is a user object and if there are no errors in the user object
@@ -20,6 +26,17 @@ function Navigation({ isLoaded }: NavigationProps) {
     if (sessionUser && !sessionUser.errors) {
         userLoggedIn = true;
     }
+
+    const handleDashBoardButton = (e: React.MouseEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        if (sessionUser?.id) navigate(`/users/${sessionUser.id}`);
+    };
+
+    const handleLogout = async (e: React.MouseEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        await dispatch(logout());
+        navigate('/', { replace: true });
+    };
 
     return (
         <header className="nav-container">
@@ -31,7 +48,30 @@ function Navigation({ isLoaded }: NavigationProps) {
                 </div>
                 <nav className="nav-links-other">
                     {isLoaded && userLoggedIn && sessionUser ? (
-                        <ProfileButton />
+                        // <ProfileButton />
+                        <div className="nav-links-login-and-signout">
+                            {!currentRoute?.includes('/users') ? (
+                                <>
+                                    <div onClick={handleDashBoardButton}>
+                                        <NavLink to={`/users/${sessionUser.id}`}>Dashboard</NavLink>
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <div>
+                                        <NavLink to="/">Home</NavLink>
+                                    </div>
+                                    <div>
+                                        <NavLink to="mailto:support@devpair.com?subject=Contact%20Us!">
+                                            Contact Us
+                                        </NavLink>
+                                    </div>
+                                </>
+                            )}
+                            <div onClick={handleLogout} style={{ cursor: 'pointer' }}>
+                                Log Out
+                            </div>
+                        </div>
                     ) : (
                         <div className="nav-links-login-and-signout">
                             <div>
